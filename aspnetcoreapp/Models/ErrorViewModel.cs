90e9af26ef49227f131aa082345d024e92b31f59
baghel2 <arvindsinghbaghel700@gmail.com>
2022-06-28 12:52:12 +0000

    first comit

---
+++ aspnetcoreapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnetcoreapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
